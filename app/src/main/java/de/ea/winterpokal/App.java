f6f48217145b41156129b27b22b48eb1c4a9eb4a
ea <evgeni.alesinskyy@gmail.com>
2017-12-30 10:40:50 +0100

    winterpokal: added global application manager to handle connectivity issues and shutdown app if no internet connection exists

--- app/src/main/java/de/ea/winterpokal/App.java
+++ app/src/main/java/de/ea/winterpokal/App.java
@@ -2,18 +2,25 @@ package de.ea.winterpokal;
 
 import de.ea.winterpokal.persistence.DAOFactory;
 import de.ea.winterpokal.persistence.DatabaseHelper;
+
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Application;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.util.Log;
 
-public class App extends Application {
+public class App extends Application    {
 
 	private static Context context;
 	private static App instance;
 	private static DAOFactory daoFactory;
+	private  LifeCycleManager lifeCycleManager;
 
 	public void onCreate() {
 		super.onCreate();
@@ -34,6 +41,8 @@ public class App extends Application {
 			G.PHONE_MODEL = android.os.Build.MODEL;
 			// Android version
 			G.ANDROID_VERSION = android.os.Build.VERSION.RELEASE;
+			lifeCycleManager = new LifeCycleManager();
+			registerActivityLifecycleCallbacks(lifeCycleManager);
 		} catch (NameNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -65,5 +74,59 @@ public class App extends Application {
 			daoFactory = DAOFactory.instance(DAOFactory.REMOTE);
 		return daoFactory;
 
+	}
+
+
+	class LifeCycleManager implements Application.ActivityLifecycleCallbacks {
+		@Override
+		public void onActivityCreated(final Activity activity, Bundle savedInstanceState) {
+
+
+		}
+
+		@Override
+		public void onActivityStarted(final Activity activity) {
+			if (!App.getInstance().isOnline()) {
+				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity);
+				// set title
+				alertDialogBuilder.setTitle(R.string.error);
+				alertDialogBuilder.setMessage(R.string.noConnection).setCancelable(false).setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+
+					public void onClick(DialogInterface dialog, int which) {
+						activity.finish();
+					}
+				});
+				// create alert dialog
+				AlertDialog alertDialog = alertDialogBuilder.create();
+				// show it
+				alertDialog.show();
+				return;
+			}
+		}
+
+		@Override
+		public void onActivityResumed(Activity activity) {
+
+		}
+
+		@Override
+		public void onActivityPaused(Activity activity) {
+
+		}
+
+		@Override
+		public void onActivityStopped(Activity activity) {
+
+		}
+
+		@Override
+		public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+		}
+
+		@Override
+		public void onActivityDestroyed(Activity activity) {
+
+		}
 	}
 }
